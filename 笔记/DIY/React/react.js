a5d73e999ae1a22453f2c3406adb33dbfd3bc749
CN\tangzhengyun <tangzhengyun@corp.netease.com>
2020-11-10 17:28:01 +0800

    fiber

--- 笔记/DIY/React/react.js
+++ 笔记/DIY/React/react.js
@@ -23,13 +23,75 @@ function createTextElement(text){
 }
 
 function render(element, container) {
-    const dom = element.type=="TEXT_ELEMENT" ? document.createTextNode('') :document.createElement(element.type)
-    element.props.children.forEach(item => {
-        render(item, dom)
-    });
+    //设置单元工作
+    nextUnitOfWork = {
+        dom: container,
+        props:{
+            children: [element]
+        }
+    }
+}
+
+function createDom(fiber) {
+    const dom = 
+    fiber.type == 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type)
     const isProperty = key=> key!== 'children'
-    Object.keys(element.props).filter(isProperty).forEach(item=>{
-        dom[item] = element.props[item]
+    Object.keys(fiber.props).filter(isProperty).forEach(item=>{
+        dom[item] = fiber.props[item]
     })
-    container.appendChild(dom)
+    return dom
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline){
+    while(nextUnitOfWork&&deadline.timeRemaining()>1) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+        console.log('nextUnitOfWork',nextUnitOfWork)
+    }
+    requestIdleCallback(workLoop)
+}
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber){
+    //生成当前fiber的dom
+    if(!fiber.dom){
+        fiber.dom = createDom(fiber)
+    }
+    if(fiber.parent){
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+
+    const elements = fiber.props.children
+    let index=0
+    let prevSibling = null
+    while(index<elements.length){
+        const element = elements[index]
+        let newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null
+        }
+
+        if (index===0){
+            fiber.child = newFiber
+        }else{
+            prevSibling.sibling = newFiber
+        }
+        prevSibling = newFiber
+        index++
+    }
+    //找nextUnitOfWork
+    if (fiber.child){
+        return fiber.child
+    }
+
+    let nextFiber = fiber
+    while(nextFiber){
+        if(nextFiber.sibling){
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
 }
