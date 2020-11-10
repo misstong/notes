36cf9209a4a98a92c4a19d6b0c9c76834116b68b
CN\tangzhengyun <tangzhengyun@corp.netease.com>
2020-11-10 20:26:17 +0800

    reconcile

--- 笔记/DIY/React/react.js
+++ 笔记/DIY/React/react.js
@@ -27,10 +27,12 @@ function render(element, container) {
     wipRoot = {
         dom: container,
         props:{
-            children: [element]
-        }
+            children: [element],
+        },
+        alternate: currentRoot
     }
     nextUnitOfWork=wipRoot
+    deletions = []
 }
 
 function createDom(fiber) {
@@ -45,6 +47,8 @@ function createDom(fiber) {
 
 let nextUnitOfWork = null
 let wipRoot = null
+let currentRoot = null
+let deletions = null
 
 function workLoop(deadline){
     while(nextUnitOfWork&&deadline.timeRemaining()>1) {
@@ -68,25 +72,7 @@ function performUnitOfWork(fiber){
     // }
 
     const elements = fiber.props.children
-    let index=0
-    let prevSibling = null
-    while(index<elements.length){
-        const element = elements[index]
-        let newFiber = {
-            type: element.type,
-            props: element.props,
-            parent: fiber,
-            dom: null
-        }
-
-        if (index===0){
-            fiber.child = newFiber
-        }else{
-            prevSibling.sibling = newFiber
-        }
-        prevSibling = newFiber
-        index++
-    }
+    reconcileChildren(fiber, elements)
     //找nextUnitOfWork
     if (fiber.child){
         return fiber.child
@@ -100,10 +86,58 @@ function performUnitOfWork(fiber){
         nextFiber = nextFiber.parent
     }
 }
+function reconcileChildren(wipFiber, elements){
+    let index=0
+    let prevSibling = null
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+    while(index<elements.length || oldFiber){
+        const element = elements[index]
+        let sameType = oldFiber&&element&& oldFiber.type === element.type
+        let newFiber =null
+        if(sameType){
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                parent: wipFiber,
+                dom: oldFiber.dom,
+                alternate: oldFiber,
+                effectTag: 'UPDATE'
+            }
+        }
+        if(!sameType&&element) {
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                parent: wipFiber,
+                dom: null,
+                alternate: null,
+                effectTag: 'PLACEMENT'
+            }
+        }
 
+        if(!sameType&&oldFiber){
+            oldFiber.effectTag = 'DELETE'
+            deletions.push(oldFiber)
+        }
+      
+        if (oldFiber){
+            oldFiber = oldFiber.sibling
+        }
+        if (index===0){
+            wipFiber.child = newFiber
+        }else{
+            prevSibling.sibling = newFiber
+        }
+        prevSibling = newFiber
+        index++
+    }
+}
 function commitRoot(){
+    deletions.map(commitWork)
     commitWork(wipRoot.child)
+    currentRoot=wipRoot
     wipRoot = null
+    
 }
 
 function commitWork(fiber){
@@ -112,7 +146,61 @@ function commitWork(fiber){
     }
 
     const parent = fiber.parent.dom
-    parent.appendChild(fiber.dom)
+    if (fiber.effectTag==='PLACEMENT'&&fiber.dom)
+     {
+         parent.appendChild(fiber.dom)
+     }else if (fiber.effectTag==='UPDATE'&&fiber.dom){
+         updateDom(fiber.dom,fiber.alternate.props, fiber.props)
+     }else if(fiber.effectTag==='DELETION'&&fiber.dom){
+         parent.removeChild(fiber.dom)
+     }
     commitWork(fiber.child)
     commitWork(fiber.sibling)
+}
+
+const isEvent = key => key.startsWith("on")
+const isProperty = key =>
+  key !== "children" && !isEvent(key)
+const isNew = (prev, next) => key =>
+  prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+function updateDom(dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+      key =>
+        !(key in nextProps) ||
+        isNew(prevProps, nextProps)(key)
+    )
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.removeEventListener(
+        eventType,
+        prevProps[name]
+      )
+    })
+// ​// Set new or changed properties
+Object.keys(nextProps)
+.filter(isProperty)
+.filter(isNew(prevProps, nextProps))
+.forEach(name => {
+  dom[name] = nextProps[name]
+})
+
+// Add event listeners
+Object.keys(nextProps)
+.filter(isEvent)
+.filter(isNew(prevProps, nextProps))
+.forEach(name => {
+  const eventType = name
+    .toLowerCase()
+    .substring(2)
+  dom.addEventListener(
+    eventType,
+    nextProps[name]
+  )
+})
 }
